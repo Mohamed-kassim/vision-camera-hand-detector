28235bc018b4f8a86191ea84dc2b28d1c32c75b2
MohamedKassim <mohamedkassim354@gmail.com>
2024-05-20 19:41:12 +0300

    Kickoff

---
+++ ios/VisionCameraHandDetector.swift
@@ -0,0 +1,8 @@
+@objc(VisionCameraHandDetector)
+class VisionCameraHandDetector: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
